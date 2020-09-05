b3825821dfb9e5c3c899fcfc613a8281b0c544a5
shadreza <45334134+shadreza@users.noreply.github.com>
2020-09-05 22:04:51 +0600

    Create Queue.java
    implementation of Queue using doubly linked list and generics

---
+++ Queue.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class QueueOfStrings<Items> {    /// using generics
+    public class node{
+        Items s=null;
+        node next=null;
+        node prev=null;
+    }
+    private node head = new node();
+    private node tail = new node();
+    private int size=0;
+    public void enqueue(Items st){
+        node tmp = new node();
+        tmp.s=st;
+        if(size==0){
+            head.next=tmp;
+            tmp.next=tail;
+            tmp.prev=head;
+            tail.prev=tmp;
+        }
+        else{
+            tmp.next=tail;
+            tmp.prev=tail.prev;
+            tail.prev.next=tmp;
+            tail.prev=tmp;
+        }
+        size++;
+    }
+    public void dequeue(){
+        if(size==0){
+            System.out.println("Queue Already Empty");
+        }
+        else{
+            node tmp = head.next;
+            head.next.next.prev=head;
+            head.next=head.next.next;
+            tmp.s=null;
+            tmp.next=null;
+            tmp.prev=null;
+            size--;
+        }
+    }
+    public void show(){
+        node it = head.next;
+        for(int i=0;i<size;i++){
+            System.out.println(it.s);
+            it=it.next;
+        }
+    }
+    public boolean IsEmpty(){
+        if(size==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public int returnsize(){
+        return size;
+    }
+    public static void main(String[] args) {
+        QueueOfStrings q = new QueueOfStrings();
+        Scanner inp = new Scanner(System.in);
+        String in = inp.next();
+        while(!in.equals("-1")){
+            if(in.equals("-")){
+                q.dequeue();
+            }
+            else{
+                q.enqueue(in);
+            }
+            q.show();
+            in =inp.next();
+        }
+    }
+}
